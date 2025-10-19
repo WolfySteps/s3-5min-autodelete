9d159130785a89e931ffe7067ee0557ddabb1c4b
WolfySteps <95564474+WolfySteps@users.noreply.github.com>
2025-10-19 01:17:14 -0700

    Update delete_object.py

--- delete_object.py
+++ delete_object.py
@@ -5,5 +5,5 @@ def handler(event, context):
     bucket = event["bucket"]
     key = event["key"]
     s3.delete_object(Bucket=bucket, Key=key)
-    return {"deleted": f"s3://{bucket}/{key}"}
+    return {"deleted": f"s3://{bucket}/uploads/{key}"}
 
