2566637c948c2e4042729d6204050e1b33a62317
WolfySteps <95564474+WolfySteps@users.noreply.github.com>
2025-10-19 00:20:32 -0700

    Update delete_object.py

--- delete_object.py
+++ delete_object.py
@@ -1,13 +1,9 @@
-
 import boto3
 s3 = boto3.client("s3")
-
-
 
 def handler(event, context):
     bucket = event["bucket"]
     key = event["key"]
-    # If bucket has versioning and you only want to delete the latest version,
-    # this is enough. To hard-delete all versions, you'd list_object_versions and loop.
     s3.delete_object(Bucket=bucket, Key=key)
     return {"deleted": f"s3://{bucket}/{key}"}
+
